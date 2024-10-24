a0ed08a35a1157d45ee2567c8587bdc740ed3d80
Sidney <angelsinhbfs@gmail.com>
2024-10-24 11:44:47 -0700

    move things to env from secrets

--- Portfolio.go
+++ Portfolio.go
@@ -4,12 +4,17 @@ import (
 	"Portfolio/db"
 	"context"
 	"log"
+	"os"
 )
 
 var DBClient *db.MongoClient
 var CTX context.Context
+var ExpectedUser string
+var ExpectedKey string
 
 func main() {
+	ExpectedUser = os.Getenv("EXPECTED_USER")
+	ExpectedKey = os.Getenv("EXPECTED_KEY")
 	var err error
 	DBClient, err = db.NewMongoClient("mongodb://localhost:27017", "Portfolio")
 	if err != nil {
